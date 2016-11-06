c364e0a26e672b0579004b1f8a38370e6a6f6d2a
damascenodiego <carlos.damasceno08@gmail.com>
2016-11-06 20:25:23 -0200

    mpi basic commands (proc 0)

--- fsmPrioritizationParallel/src/fsmPrioritization.cpp
+++ fsmPrioritizationParallel/src/fsmPrioritization.cpp
@@ -11,7 +11,7 @@
 #include "lib/fsmLib.h"
 #include <mpi.h>
 #include <omp.h>
-
+#include <unistd.h>
 
 using namespace std;
 
@@ -20,69 +20,79 @@ void printTest(FsmTestSuite *fsmTest);
 
 int main(int argc, char **argv) {
 
+	int         my_rank, num_proc;
+
 	MPI::Init(argc, argv);
-	FILE *fsmFile;
-	FILE *testFile;
-	FILE *testPrtzFile;
+	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &num_proc);
+
+	if (my_rank == 0) {
+		FILE *fsmFile;
+		FILE *testFile;
+		FILE *testPrtzFile;
 
 
-	FsmModel *fsmModel;
-	FsmTestSuite *fsmTest;
+		FsmModel *fsmModel;
+		FsmTestSuite *fsmTest;
 
 
-	if(argc >= 3){
-		fsmFile = fopen(argv[1],"r");
-		fsmModel = loadFsm(fsmFile);
-		fclose(fsmFile);
+		if(argc >= 3){
+			fsmFile = fopen(argv[1],"r");
+			fsmModel = loadFsm(fsmFile);
+			fclose(fsmFile);
 
-		testFile = fopen(argv[2],"r");
-		fsmTest = loadTest(testFile,fsmModel);
-		fclose(testFile);
-	}else{
-		return (1);
-	}
+			testFile = fopen(argv[2],"r");
+			fsmTest = loadTest(testFile,fsmModel);
+			fclose(testFile);
+		}else{
+			return (1);
+		}
 
 
-	char * prtz = (char *)calloc(1,sizeof(char)*(strlen(argv[2])+14));
-	{
+		char * prtz = (char *)calloc(1,sizeof(char)*(strlen(argv[2])+14));
+//		{
+//			strcat(prtz,argv[2]);
+//			strcat(prtz,".cov");
+//			FILE *testCoverageFile = fopen(prtz,"w");
+//			saveTestCoverage(testCoverageFile,fsmTest);
+//			fflush(testCoverageFile);
+//			fclose(testCoverageFile);
+//
+//		}
+//		free(prtz);
+//		prtz = (char *)calloc(1,sizeof(char)*(strlen(argv[2])+14));
+
 		strcat(prtz,argv[2]);
-		strcat(prtz,".cov");
-		FILE *testCoverageFile = fopen(prtz,"w");
-		saveTestCoverage(testCoverageFile,fsmTest);
-		fflush(testCoverageFile);
-		fclose(testCoverageFile);
+		if(argc > 3){
+			strcat(prtz,".gmdp.test");
+			prioritization_gmdp(fsmTest);
+		}else{
+			strcat(prtz,".lmdp.test");
+			prioritization_lmdp(fsmTest);
+		}
+
+		testPrtzFile = fopen(prtz,"w");
+		saveTest(testPrtzFile,fsmTest);
+		fclose(testPrtzFile);
+
+		{
+			strcat(prtz,".cov");
+			FILE *testCoverageFile = fopen(prtz,"w");
+			saveTestCoverage(testCoverageFile,fsmTest);
+			fclose(testCoverageFile);
+
+		}
+		delete(fsmModel);
+		delete(fsmTest);
+
+		int rank = MPI::COMM_WORLD.Get_rank();
+		int cnt = omp_get_thread_num();
+		printf("MPI::COMM_WORLD.Get_rank(): %d\n",rank);
+		printf("omp_get_thread_num(): %d\n",cnt);
 
 	}
-	free(prtz);
-	prtz = (char *)calloc(1,sizeof(char)*(strlen(argv[2])+14));
 
-	strcat(prtz,argv[2]);
-	if(argc > 3){
-		strcat(prtz,".gmdp.test");
-		prioritization_gmdp(fsmTest);
-	}else{
-		strcat(prtz,".lmdp.test");
-		prioritization_lmdp(fsmTest);
-	}
-
-	testPrtzFile = fopen(prtz,"w");
-	saveTest(testPrtzFile,fsmTest);
-	fclose(testPrtzFile);
-
-	{
-		strcat(prtz,".cov");
-		FILE *testCoverageFile = fopen(prtz,"w");
-		saveTestCoverage(testCoverageFile,fsmTest);
-		fclose(testCoverageFile);
-
-	}
-	delete(fsmModel);
-	delete(fsmTest);
-
-	int rank = MPI::COMM_WORLD.Get_rank();
-	int cnt = omp_get_thread_num();
-	printf("MPI::COMM_WORLD.Get_rank(): %d\n",rank);
-	printf("omp_get_thread_num(): %d\n",cnt);
+	//sleep (10); //Sleeps for 1000 ms or 1 sec
 
 	MPI::Finalize();
 	return 0;
