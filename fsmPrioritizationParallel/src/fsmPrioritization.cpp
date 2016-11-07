6abc4342d3bd9e33b8fcb942eb58de7a0b95449b
damascenodiego <carlos.damasceno08@gmail.com>
2016-11-07 11:32:35 -0200

    parallel_lmdp: spawn ([n(n-1)]/2) processes

--- fsmPrioritizationParallel/src/fsmPrioritization.cpp
+++ fsmPrioritizationParallel/src/fsmPrioritization.cpp
@@ -48,55 +48,110 @@ int main(int argc, char **argv) {
 			return (1);
 		}
 
+		char 	command[20], **argv_spawn;
+		int *array_of_errcodes;
+		int color = my_rank;
+		int key = my_rank;
+		MPI_Comm	cij_comm,intercomm;
 
-		char * prtz = (char *)calloc(1,sizeof(char)*(strlen(argv[2])+14));
-//		{
-//			strcat(prtz,argv[2]);
-//			strcat(prtz,".cov");
-//			FILE *testCoverageFile = fopen(prtz,"w");
-//			saveTestCoverage(testCoverageFile,fsmTest);
-//			fflush(testCoverageFile);
-//			fclose(testCoverageFile);
-//
-//		}
-//		free(prtz);
-//		prtz = (char *)calloc(1,sizeof(char)*(strlen(argv[2])+14));
+		MPI_Comm_split(MPI_COMM_WORLD, color, key, &cij_comm);
 
-		strcat(prtz,argv[2]);
-		if(argc > 3){
-			strcat(prtz,".gmdp.test");
-			prioritization_gmdp(fsmTest);
-		}else{
-			strcat(prtz,".lmdp.test");
-			prioritization_lmdp(fsmTest);
+		strcpy(command, "lmdp");
+		argv_spawn = (char **)0;
+		int nn_1_2 = (int)((fsmTest->getNoResets()*(fsmTest->getNoResets()-1))/2);
+
+		// used by MPI_Comm_spawn
+		array_of_errcodes = (int *) calloc(nn_1_2, sizeof(int));
+
+
+		int root = 0;
+		MPI_Comm_spawn(command, argv_spawn, nn_1_2, MPI_INFO_NULL, root, cij_comm, &intercomm, array_of_errcodes);
+
+		MPI_Request * mpirequest = (MPI_Request *) calloc(nn_1_2, sizeof(MPI_Request));
+
+		std:: list<FsmTestCase*> t;
+		for(FsmTestCase *i : fsmTest->getTestCase()){
+			t.push_back(i);
 		}
 
-		testPrtzFile = fopen(prtz,"w");
-		saveTest(testPrtzFile,fsmTest);
-		fclose(testPrtzFile);
+		std::list<FsmTestCase*>::iterator endi = t.end(); endi--;
+		std::list<FsmTestCase*>::iterator endj = t.end();;
 
-		{
-			strcat(prtz,".cov");
-			FILE *testCoverageFile = fopen(prtz,"w");
-			saveTestCoverage(testCoverageFile,fsmTest);
-			fclose(testCoverageFile);
+		int tag = 0;
+		int dest = 0;
+		SimpleFsmTestCase *sf = nullptr;
+		for(auto ti = t.begin(); ti != endi; ti++){
+			auto tj = ti;
+			for(tj++; tj != endj; tj++){
+				sf = (*ti)->getSimpleFormat();
+				MPI_Send(&(sf->testId),1,MPI_INT, dest, tag, intercomm);
+				MPI_Send(&(sf->pTot),1,MPI_INT, dest, tag, intercomm);
+				MPI_Send(sf->p,sf->pTot,MPI_INT, dest, tag, intercomm);
 
+				sf = (*tj)->getSimpleFormat();
+				MPI_Send(&(sf->testId),1,MPI_INT, dest, tag, intercomm);
+				MPI_Send(&(sf->pTot),1,MPI_INT, dest, tag, intercomm);
+				MPI_Send(sf->p,sf->pTot,MPI_INT, dest, tag, intercomm);
+
+				dest++;
+			}
 		}
+		struct {
+		        double val;
+		        int   rank;
+		} sendbuf,rcvbuf;
+
+		MPI_Reduce(&sendbuf, &rcvbuf, 1, MPI_DOUBLE_INT, MPI_MAXLOC, MPI_ROOT, intercomm);
+		printf("MAX_LOC.val:%f\n", rcvbuf.val);
+		printf("MAX_LOC.rank:%d\n", rcvbuf.rank);
+		//	    sleep(5);
+
+		//		char * prtz = (char *)calloc(1,sizeof(char)*(strlen(argv[2])+14));
+		//		{
+		//			strcat(prtz,argv[2]);
+		//			strcat(prtz,".cov");
+		//			FILE *testCoverageFile = fopen(prtz,"w");
+		//			saveTestCoverage(testCoverageFile,fsmTest);
+		//			fflush(testCoverageFile);
+		//			fclose(testCoverageFile);
+		//
+		//		}
+		//		free(prtz);
+		//		prtz = (char *)calloc(1,sizeof(char)*(strlen(argv[2])+14));
+		//
+		//		strcat(prtz,argv[2]);
+		//		if(argc > 3){
+		//			strcat(prtz,".gmdp.test");
+		//			prioritization_gmdp(fsmTest);
+		//		}else{
+		//			strcat(prtz,".lmdp.test");
+		//		prioritization_lmdp(fsmTest);
+		//		}
+		//
+		//		testPrtzFile = fopen(prtz,"w");
+		//		saveTest(testPrtzFile,fsmTest);
+		//		fclose(testPrtzFile);
+		//
+		//		{
+		//			strcat(prtz,".cov");
+		//			FILE *testCoverageFile = fopen(prtz,"w");
+		//			saveTestCoverage(testCoverageFile,fsmTest);
+		//			fclose(testCoverageFile);
+		//
+		//		}
 		delete(fsmModel);
 		delete(fsmTest);
 
-		int rank = MPI::COMM_WORLD.Get_rank();
-		int cnt = omp_get_thread_num();
-		printf("MPI::COMM_WORLD.Get_rank(): %d\n",rank);
-		printf("omp_get_thread_num(): %d\n",cnt);
+//		int rank = MPI::COMM_WORLD.Get_rank();
+//		int cnt = omp_get_thread_num();
+//		printf("MPI::COMM_WORLD.Get_rank(): %d\n",rank);
+//		printf("omp_get_thread_num(): %d\n",cnt);
 
 	}
 
-	//sleep (10); //Sleeps for 1000 ms or 1 sec
-
 	MPI::Finalize();
-	return 0;
-
+	printf("\n\n--- THE END (%d)!!!---",my_rank);
+	exit(0);
 }
 
 
