c3f6b1511343df5be1d850a2e216e05bfaa58ad4
damascenodiego <carlos.damasceno08@gmail.com>
2016-11-22 15:10:58 -0200

    single host experiment script adjusted

--- experimentSinglehost.sh
+++ experimentSinglehost.sh
@@ -7,6 +7,7 @@ fsm="./tests/Masood2009P2v2.kk"
 test="./tests/Masood2009P2v2.spy.test"
 
 siz=10
+np=2
 
 for siz in `seq 10 10 100` `seq 200 100 1000` `seq 2000 1000 10000`; do
    for np in `seq 2 8`; do
@@ -18,7 +19,7 @@ for siz in `seq 10 10 100` `seq 200 100 1000` `seq 2000 1000 10000`; do
 #         md5sum $test.$siz.test*gmdp.test |sort | cut -d. -f1 |uniq | wc -l
 
          ./lmdp $fsm $test.$siz.test #>> /dev/null 
-         mpirun -np $np ./plmdp $fsm $test.$siz.test |#>> /dev/null 
+         mpirun -np $np ./plmdp $fsm $test.$siz.test #>> /dev/null 
 #         md5sum $test.$siz.test*lmdp.test |sort | cut -d. -f1 |uniq | wc -l
          
 #        sleep 1s
