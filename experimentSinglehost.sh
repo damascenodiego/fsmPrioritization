1f0f1894ae155c28e2b6aab38164dd921dd939c5
damascenodiego <carlos.damasceno08@gmail.com>
2016-11-22 15:13:09 -0200

    single host experiment

--- experimentSinglehost.sh
+++ experimentSinglehost.sh
@@ -7,7 +7,6 @@ fsm="./tests/Masood2009P2v2.kk"
 test="./tests/Masood2009P2v2.spy.test"
 
 siz=10
-np=2
 
 for siz in `seq 10 10 100` `seq 200 100 1000` `seq 2000 1000 10000`; do
    for np in `seq 2 8`; do
