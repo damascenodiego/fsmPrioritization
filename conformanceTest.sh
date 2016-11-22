064b8bd6435672f3fa8f0aebb15c6dae8da19290
damascenodiego <carlos.damasceno08@gmail.com>
2016-11-22 16:25:31 -0200

    conformanceTest.sh file description include as comments

--- conformanceTest.sh
+++ conformanceTest.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# This script executes test prioritization using all four algorithms over a same test suite.
+# The prioritized test suite is saved as text file.
+# The md5 hash value is used to show the conformance between the serial/parallel prioritization algorithms.
+# If "cut -d. -f1 |uniq | wc -l" command outputs '1', then md5 of both prioritized test suites is the same.
+# Thus, there is a valid conformance relationship between the serial/parallel test prioritization algorithms.
+
 cd `dirname $0`
 
 
@@ -15,11 +21,11 @@ for siz in `seq 10 50`; do
 
          ./gmdp $fsm $test.$siz.test >> /dev/null 
          mpirun -np $np ./pgmdp $fsm $test.$siz.test  >> /dev/null
-         md5sum $test.$siz.test*gmdp.test |sort | cut -d. -f1 |uniq | wc -l
+         md5sum $test.$siz.test*gmdp.test |sort | cut -d. -f1 |uniq | wc -l # conformance relationship between gmdp and pgmdp
 
          ./lmdp $fsm $test.$siz.test >> /dev/null 
          mpirun -np $np ./plmdp $fsm $test.$siz.test >> /dev/null 
-         md5sum $test.$siz.test*lmdp.test |sort | cut -d. -f1 |uniq | wc -l
+         md5sum $test.$siz.test*lmdp.test |sort | cut -d. -f1 |uniq | wc -l # conformance relationship between lmdp and plmdp
          
          sleep 1s
          clear
